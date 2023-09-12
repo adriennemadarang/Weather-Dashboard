ef9a63eaab2344689f734f18f901b0e51e3ebf26
adriennemadarang <adriennemadarang@icloud.com>
2023-09-11 23:25:02 -0700

    fixed weather api

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-var cityInputEl = document.querySelector("#city-input");
-var searchForm = document.querySelector("#search-form");
-var clearButton = document.querySelector("#clear-button");
+var cityInput = document.querySelector("#city-input");
+var searchButton = $('#search-button');
+var clearButton = $('#clear-button');
 var presentDayForecast = document.querySelector("#present-day-forecast");
 var fiveDayForecast = document.querySelector("#five-day-forecast");
-var pastSearch = 
-var searchHistory
+
+var pastSearches = $('#past-searches');
+
+
 
 function dashboard(event) {
     event.preventDefault();
@@ -12,16 +14,8 @@ function dashboard(event) {
     displayWeather(cityName);
 }
 
-function displayWeather(cityName) {
-    var url = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=07b4242051c9c905646e12bcdbddb273";
-    fetch(url)
-}
-
-// function clearSearchHistory() {
-//     localStorage.clear();
-// pastSearch.innerHTML = "";
-// searchHistory = [];
-// }
-// clearButton.addEventListener("click", function () {
-//     clearSearchHistory();
-// }); 
+function displayWeather(data) {
+    var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=7b4242051c9c905646e12bcdbddb273';
+    fetch(requestUrl)
+     
+    }
