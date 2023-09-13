3c5bfd9f169d76d7ec8541bc9374a2b5567070d3
adriennemadarang <adriennemadarang@icloud.com>
2023-09-12 20:16:17 -0700

    fixed current city details js

--- script.js
+++ script.js
@@ -1,21 +1,38 @@
-var cityInput = document.querySelector("#city-input");
-var searchButton = $('#search-button');
-var clearButton = $('#clear-button');
-var presentDayForecast = document.querySelector("#present-day-forecast");
-var fiveDayForecast = document.querySelector("#five-day-forecast");
-
-var pastSearches = $('#past-searches');
+var today = moment().format('L');
+var apiKey = '51c7b424209c905646e12bcdbddb273';
+var searchHistory = [];
 
 
+function displayWeather(city) {
+    var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}';
+   
+    $.ajax({
+        url: requestUrl,
+        method: "GET"
+    }).then(function(cityWeatherResponse){
+        console.log(cityWeatherResponse);
 
-function dashboard(event) {
-    event.preventDefault();
-    var cityName = cityInputEl.value;
-    displayWeather(cityName);
-}
+        $("#weatherContent").css("display", "block");
+        $("#cityDetails").empty();
 
-function displayWeather(data) {
-    var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=7b4242051c9c905646e12bcdbddb273';
-    fetch(requestUrl)
+        var iconCode = cityWeatherResponse.weather[0].icon;
+        var iconUrl = 'https://openweathermap.org/img/w/${iconCode}.png';
+        var currentCity = $(`
+        <h2 id="currentCity">
+        ${cityWeatherResponse.name} ${today} <img src="${iconUrl}" alt="${cityWeatherResponse.weather[0].description}"/>
+        </h2>
+        <p>Temperature: ${cityWeatherResponse.main.temp} °F</p>
+        <p>WindSpeed: ${cityWeatherResponse.wind.speed} MPH</p>
+        <p>Humidity: ${cityWeatherResponse.main.humidity} %</p>
+        `);
+
+        $("#cityDetails").append(currentCity);
+
+        var lat = cityWeatherResponse.coord.lat;
+        var lon = cityWeatherResponse.coord.lon;
+        var uvIndexQuery = 'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}';
+
+        
+    })
      
     }
