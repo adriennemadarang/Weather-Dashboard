01c02430ca2fb32d0532c102e1e37c6515f4c52d
adriennemadarang <adriennemadarang@icloud.com>
2023-09-12 21:50:53 -0700

    added five day forecast

--- script.js
+++ script.js
@@ -30,9 +30,76 @@ function displayWeather(city) {
 
         var lat = cityWeatherResponse.coord.lat;
         var lon = cityWeatherResponse.coord.lon;
-        var uvIndexQuery = 'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}';
+        var uvIndexUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}';
 
-        
+        $.ajax({
+            url: uvIndexUrl,
+            method: "GET"
+        }).then(function(uviResponse){
+            console.log(uviResponse);
+
+            var uvIndex = uviResponse.value;
+            var uvIndexP = $(`
+            <p>UV Index: 
+            <span id="uvIndexcolor" class="px-2 py-2 rounded">${uvIndex}</span
+            </p>
+        `);
+
+        $("#cityDetails").append(uvIndexP);
+
+        futureWeather(lat, lon);
+        if (uvIndex >= 0 && uvIndex <= 2){
+            $("#uvIndexColor").css("background-color", "#3EA72D").css("color", "white");
+        } else if (uvIndex >= 3 && uvIndex <= 5) {
+            $("#uvIndexColor").css("background-color", "#FFF300");
+        } else if (uvIndex >=6 && uvIndex <=7){
+            $("#uvIndexColor").css("background-color", "#F18B00");
+        } else if (uvIndex >= 8 && uvIndex <= 10) {
+            $("#uvIndexColor").css("background-color", "#E53210").css("color", "white");
+        } else {
+            $("#uvIndexColor").css("background-color", "#B567A4").css("color", "white");
+        };
+    }); 
+});
+
+function futureWeather(lat, lon) {
+    var futureWeatherURL = 'https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=current,minutely,hourly,alerts&appid=${apiKey';
+
+    $.ajax({
+        url: futureWeatherURL,
+        method: "GET"
+    }).then(function(fiveDayResponse){
+        console.log(fiveDayResponse);
+        $("#fiveDay").empty();
+
+        for (let i = 1; i < 6; i++){
+            var cityInfo = {
+                date: fiveDayResponse.daily[i].dt,
+                icon: fiveDayResponse.daily[i].weather[0].icon,
+                temp: fiveDayResponse.daily[i].temp.day,
+                humidity: fiveDayResponse.daily[i].humidity
+            };
+
+            var currentDate = moment.unix(cityInfo.date).format("MM/DD/YYYY");
+            var iconURL = '<img src="https://openweathermap.org/img/w/${cityInfo.icon}.png alt="${futureResponse.daily[i].weather[0].main}"/>';
+            var futureCard = $(`
+            <div class="pl-3">
+                <div class="card pl-3 pt-3 mb-3 bg-primary text-light" style="width: 12rem">
+                    <div class="card-body">
+                    <h5>${currentDate}</h5>
+                    <p>$${iconURL}</p>
+                    <p>Temperature: ${cityInfo.temp} °F </p>
+                    <p>Humidity: ${cityInfo.humidity} %</p>
+                </div>
+            </div>
+        </div>
+            `);
+            $("fiveDay").append(futureCard);
+        }
+    });
+}
+
+
     })
      
     }
